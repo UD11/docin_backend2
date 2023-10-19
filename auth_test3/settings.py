670c9fb7e2a3dd9221eef9b1a679bf53d3fff0fa
UD <sealuddalak84@gmail.com>
2023-10-20 01:53:11 +0530

    d

--- auth_test3/settings.py
+++ auth_test3/settings.py
@@ -68,12 +68,12 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    # "http://localhost:3000",
-    "https://voyager-git-main-ud11.vercel.app/"
-]
+# CORS_ALLOWED_ORIGINS = [
+#     # "http://localhost:3000",
+#     "https://voyager-git-main-ud11.vercel.app/"
+# ]
 
-# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = True
 
 
 ROOT_URLCONF = 'auth_test3.urls'
